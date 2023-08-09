0aa91da453dc2e13b0dd031d1e94de07599f9c27
n106ky <n106ky@gmail.com>
2023-08-09 19:39:09 -0400

    Updated Comments Forms and About

--- js/reviews.js
+++ js/reviews.js
@@ -1,7 +1,7 @@
 
 window.reviews = [
   {
-    id: 1,
+    //id: 1,
     name: "Wavierwww",
     date: "2023-8-6",
     imageUrl: `https://pbs.twimg.com/profile_images/1354154947630292993/nGzZq5MN_400x400.jpg`,
@@ -9,7 +9,7 @@ window.reviews = [
     ratings: 4
   },
   {
-    id: 2,
+    //id: 2,
     name: "Lake",
     date: "2023-8-2",
     imageUrl: `https://static.wikia.nocookie.net/4503bd5c-072f-44fe-bb1c-40db1c016385/scale-to-width/755`,
@@ -17,7 +17,7 @@ window.reviews = [
     ratings: 5
   },
   {
-    id: 3,
+    //id: 3,
     name: "DrewDude",
     date: "2023-7-19",
     imageUrl: `https://images.squarespace-cdn.com/content/v1/5ab85e1fb98a7888d761a856/1539290385703-39AT8H3MKU130V9HWGK1/bwg_square_v1.jpg`,
@@ -25,7 +25,7 @@ window.reviews = [
     ratings: 5
   },
   {
-    id: 4,
+    //id: 4,
     name: "Hehe999",
     date: "2023-5-21",
     imageUrl: `https://static01.nyt.com/images/2021/04/30/multimedia/30xp-meme/29xp-meme-mediumSquareAt3X-v5.jpg`,
@@ -33,7 +33,7 @@ window.reviews = [
     ratings: 5
   },
   {
-    id: 5,
+    //id: 5,
     name: "ConfusedLady",
     date: "2022-12-29",
     imageUrl: `https://64.media.tumblr.com/99b3aa3543c285ec379d0e4c6063427c/917fb3f20dfb2f12-49/s400x600/1f6a1f4f2dca6ad7ccfedfabef410495ea9f16a9.gifv`,
@@ -41,7 +41,7 @@ window.reviews = [
     ratings: 4
   },
   {
-    id: 6,
+    //id: 6,
     name: "SingingCat",
     date: "2022-10-6",
     imageUrl: `https://as2.ftcdn.net/v2/jpg/01/21/53/67/1000_F_121536712_G4dCFgDHNl4Y2MJQjjXH3wprpC7jxXd4.jpg`,
@@ -52,9 +52,11 @@ window.reviews = [
 
 
 const reviews = window.reviews;
-function createReviews() {
+function displayReviews() {
   const reviewsDiv = document.getElementById("reviewsDiv");
+  reviewsDiv.innerHTML = "";
   const header = document.createElement("h2");
+  header.classList.add("headerh2");
   header.textContent = "#CommentsArea";
 
   const cardContainer = document.createElement("div");
@@ -95,6 +97,38 @@ function createReviews() {
     infoContainer.appendChild(ratings);
     });
 }
-createReviews();
+// displayReviews();
 
 
+function addReview() {
+  var form = document.getElementById("commentsForm");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    var name = document.getElementById("name").value;
+    var date = new Date();
+    var formattedDate = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+    var imageUrl= `https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQFH1wgOpFSsG2R9yXCMr5iC_tZ7uD4QNNZFeFyAT4WLAdMow4H`;
+    var comment = document.getElementById("comments").value; // Changed to "comments"
+    var ratings = document.getElementById("ratings").value;
+
+    var newReview = {
+      name,
+      date: formattedDate, // assigning the value of the formattedDate variable to the date property of the newReview object. Just "formattedDate" is wrong.
+      imageUrl,
+      comment,
+      ratings,
+    };
+
+    reviews.push(newReview);
+    form.reset();
+    displayReviews();
+  });
+}
+
+function onLoad() {
+  displayReviews();
+  addReview();
+}
+
+window.addEventListener("load", onLoad);
